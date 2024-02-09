8542e61149669b5134f3b914606667258dace155
missminyoon3 <47670662+missminyoon3@users.noreply.github.com>
2024-02-09 12:57:43 +0300

    Initial Commit

---
+++ movieBook/movieBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieBookApp.swift
+//  movieBook
+//
+//  Created by MacBook Pro on 9.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct movieBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
